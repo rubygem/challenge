3f8a54c5499d42b4fb768319ee90b3d0614a5f26
rubygem <gemma.cameron@gmail.com>
2015-06-23 14:32:36 +0100

    refactoring

--- task1/tests/test_electricity_generation_fields.rb
+++ task1/tests/test_electricity_generation_fields.rb
@@ -14,19 +14,17 @@ class AllTheTests < Test::Unit::TestCase
 	# 	```
 	# 	PN,T_RATS-1,5,20110712010000,475.000,20110712011800,300.000
 	# 	```
-	def test_read_first_line
-		first_line = CSVFile.new.lines[0] 
-		assert_equal "PN,T_ABTH7,5,20110712010000,485.000,20110712013000,485.000", first_line
+
+	def line
+		CSVFile.new.lines[0]
 	end
 
 	def test_read_physical_notification
-		line = CSVFile.new.lines[0]
 		generated_electricity = ElectricityGeneration.new(line:line)
 		assert_equal "PN", generated_electricity.physical_notification
 	end
 
 	def test_read_unit_id
-		line = CSVFile.new.lines[0]
 		generated_electricity = ElectricityGeneration.new(line:line)
 		assert_equal "T_ABTH7", generated_electricity.unit_id
 	end
--- task1/tests/test_read_csv_file.rb
+++ task1/tests/test_read_csv_file.rb
@@ -2,7 +2,7 @@ require 'bundler/setup'
 require 'test/unit'
 require_relative('../src/CSVFile')
 
-class AllTheTests < Test::Unit::TestCase
+class ReadingFileTests < Test::Unit::TestCase
 	def test_read_first_line
 		first_line = CSVFile.new.lines[0] 
 		assert_equal "PN,T_ABTH7,5,20110712010000,485.000,20110712013000,485.000", first_line
