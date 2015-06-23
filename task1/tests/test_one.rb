72831dfdd3777c67d665fc45b189ab2a8a2ca1fb
rubygem <gemma.cameron@gmail.com>
2015-06-23 14:21:44 +0100

    refactoring

--- task1/tests/test_one.rb
+++ task1/tests/test_one.rb
@@ -37,15 +37,3 @@ class AllTheTests < Test::Unit::TestCase
 		csv = line.split(',')
 	end
 end
-
-class CSVFile
-	NEW_LINES = /\r?\n/
-	def initialize location:"../../data", file_name:"tib_messages_FPN.2011-07-12"
-		csv_file = File.expand_path("#{location}/#{file_name}", __FILE__)
-		@csv = File.read(csv_file)
-	end
-
-	def lines 
-		@csv.split(NEW_LINES)
-	end
-end
