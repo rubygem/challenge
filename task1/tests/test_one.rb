80a20a1f37bbd42d64a2ad28227092f8d8c79d75
rubygem <gemma.cameron@gmail.com>
2015-06-23 13:52:30 +0100

    refactoring

--- task1/tests/test_one.rb
+++ task1/tests/test_one.rb
@@ -37,11 +37,16 @@ class AllTheTests < Test::Unit::TestCase
 	end
 
 	def lines_of_csv_file 
-		electricity_generation = File.read(csv_file)
-		electricity_generation.split(/\r?\n/)
+		csv = File.read(csv_file)
+		new_lines = /\r?\n/
+		csv.split(new_lines)
 	end
 
 	def csv_file location:"../../data", file_name:"tib_messages_FPN.2011-07-12"
 		File.expand_path("#{location}/#{file_name}", __FILE__)
 	end
+end
+
+class CSVFile
+
 end
