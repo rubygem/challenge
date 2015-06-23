8a145d0afed14727358ba0522251fc844802efc1
rubygem <gemma.cameron@gmail.com>
2015-06-23 12:47:42 +0100

    now reads the file

--- task1/tests/test_one.rb
+++ task1/tests/test_one.rb
@@ -3,6 +3,8 @@ require 'test/unit'
 
 class AllTheTests < Test::Unit::TestCase
 	def test_something
+		csv_file = File.expand_path('../../data/tib_messages_FPN.2011-07-12', __FILE__)
+		csv = File.read(csv_file) 
 		assert_equal 0, 0
 	end
 end
