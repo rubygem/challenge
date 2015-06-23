8a26b7b0df27c4c2c828351a9770204685a5ead3
rubygem <gemma.cameron@gmail.com>
2015-06-23 13:13:39 +0100

    refactoring

--- task1/tests/test_one.rb
+++ task1/tests/test_one.rb
@@ -3,7 +3,6 @@ require 'test/unit'
 
 class AllTheTests < Test::Unit::TestCase
 
-	# 1. `PN` - Physical Notification. All rows are `PN` rows so you can ignore this field.
 	# 2. `T_RATS-1` - Unit id. A unique identifier for the generation unit (power station boiler, wind turbine, etc).
 	# 3. `5` - The "settlement period". You can ignore this field.
 	# 4. `20110712010000` - Start date/time.
@@ -16,21 +15,22 @@ class AllTheTests < Test::Unit::TestCase
 	# 	PN,T_RATS-1,5,20110712010000,475.000,20110712011800,300.000
 	# 	```
 	def test_read_first_line
-		csv_file = File.expand_path('../../data/tib_messages_FPN.2011-07-12', __FILE__)
-		electricity_generation = File.read(csv_file)
-		lines_of_csv_file = electricity_generation.split(/\r?\n/)
 		first_line = lines_of_csv_file[0] 
 		assert_equal "PN,T_ABTH7,5,20110712010000,485.000,20110712013000,485.000", first_line
 	end
 
+	# 1. `PN` - Physical Notification. All rows are `PN` rows so you can ignore this field.
 	def test_read_physical_notification
-		csv_file = File.expand_path('../../data/tib_messages_FPN.2011-07-12', __FILE__)
-		electricity_generation = File.read(csv_file)
-		lines_of_csv_file = electricity_generation.split(/\r?\n/)
 		first_line = lines_of_csv_file[0] 
 		assert_equal "PN,T_ABTH7,5,20110712010000,485.000,20110712013000,485.000", first_line
 		csv = first_line.split(',')
 		physical_notification = csv[0]
 		assert_equal "PN", physical_notification
+	end
+
+	def lines_of_csv_file 
+		csv_file = File.expand_path('../../data/tib_messages_FPN.2011-07-12', __FILE__)
+		electricity_generation = File.read(csv_file)
+		electricity_generation.split(/\r?\n/)
 	end
 end
