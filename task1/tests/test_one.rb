1298170c96731bbab351b9417ba83ddd6ef00a8b
rubygem <gemma.cameron@gmail.com>
2015-06-23 12:50:44 +0100

    proving it

--- task1/tests/test_one.rb
+++ task1/tests/test_one.rb
@@ -4,7 +4,9 @@ require 'test/unit'
 class AllTheTests < Test::Unit::TestCase
 	def test_something
 		csv_file = File.expand_path('../../data/tib_messages_FPN.2011-07-12', __FILE__)
-		csv = File.read(csv_file) 
-		assert_equal 0, 0
+		csv = File.read(csv_file)
+		lines_of_csv_file = csv.split(/\r?\n/)
+		first_line = lines_of_csv_file[0] 
+		assert_equal "PN,T_ABTH7,5,20110712010000,485.000,20110712013000,485.000", first_line
 	end
 end
