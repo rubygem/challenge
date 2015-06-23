edab404768777ed621ba4696013c50164a796b0d
rubygem <gemma.cameron@gmail.com>
2015-06-23 12:33:07 +0100

    adding a rake file, gemfile and extracted the csv file

---
+++ task1/rakefile.rb
@@ -0,0 +1,32 @@
+@run_options = {verbose: Rake.application.options.trace}
+task :default => [:ruby_dependencies, :test, :git]
+
+task :ruby_dependencies do
+	sh 'bundle install --path gems'
+end
+
+task :test => :set_env do
+	Dir.glob('./tests/**/*.rb').each do |test|
+		clear_db
+		rake_sh 'ruby '+ test
+	end
+end
+
+task :set_env do
+end
+
+task :git => :ruby_dependencies do 
+	require 'bundler/setup'
+	require 'git_repository'
+	message = ENV['m']
+	raise 'no commit message specified' if message.nil?
+	git = GitRepository.new
+	git.pull
+	git.add({:options => '-A'})
+	git.commit(message: message )
+	git.push
+end
+
+def rake_sh(command)
+	sh command, @run_options
+end
