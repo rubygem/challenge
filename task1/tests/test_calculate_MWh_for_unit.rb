4a530369c2e7b9363ddc6cc9135a566f437eac2f
rubygem <gemma.cameron@gmail.com>
2015-06-24 10:56:50 +0100

    starting to figure out how many unique units there are

--- task1/tests/test_calculate_MWh_for_unit.rb
+++ task1/tests/test_calculate_MWh_for_unit.rb
@@ -5,10 +5,20 @@ require_relative('../src/ElectricityGeneration')
 
 class TestCalculateMegaWattHours < Test::Unit::TestCase
 	def test_total_number_of_entries_in_file
-		entries_for_unit = 0
 		total_entries = CSVFile.new.lines
 		total_number_of_entries = 40947
 		assert_equal total_number_of_entries, total_entries.count
+	end
+
+	def test_number_of_unique_units
+		total_entries = CSVFile.new.lines
+		number_of_unique_units = 826
+		units = []
+		total_entries.each do |line|
+			electricity_generated = ElectricityGeneration.new(line:line)
+			units.push electricity_generated.unit_id
+		end
+		assert_equal number_of_unique_units, units.uniq.count
 	end
 
 	def test_mega_watt_hours
