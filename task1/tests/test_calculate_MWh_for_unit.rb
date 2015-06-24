874478e4b9663147c0cb9e0b0a10eb3a21d12f8f
rubygem <gemma.cameron@gmail.com>
2015-06-24 13:27:59 +0100

    refactoring so can move out of the test

--- task1/tests/test_calculate_MWh_for_unit.rb
+++ task1/tests/test_calculate_MWh_for_unit.rb
@@ -5,28 +5,11 @@ require_relative('../src/CSVFile')
 require_relative('../src/ElectricityGeneration')
 
 class TestCalculateMegaWattHours < Test::Unit::TestCase
-	def process total_entries:
-		@units = []
-		electricity_generated_this_day = []
-		#parallel each here actually increases the time to 50 seconds from 17
-		total_entries.each do |line|
-			electricity_generated = ElectricityGeneration.new(line:line)
-			electricity_generated_this_day.push electricity_generated
-			@units.push({id:electricity_generated.unit_id}) unless @units.include?({id:electricity_generated.unit_id})
-		end
-
-		@units.peach do |unit|
-			unit[:entries] = electricity_generated_this_day.select do |electricity_generated| 
-				electricity_generated.unit_id.eql? unit[:id] 
-			end
-		end
-	end
-
 	def test_number_of_entries_per_unit
-		process total_entries:CSVFile.new.lines
+		units = Bob.new.process(total_entries:CSVFile.new.lines)
 
-		assert_equal 826, @units.count
-		sorted = @units.sort_by do |unit|
+		assert_equal 826, units.count
+		sorted = units.sort_by do |unit|
 			unit[:id]
 		end
 
@@ -35,5 +18,26 @@ class TestCalculateMegaWattHours < Test::Unit::TestCase
 		assert_equal "2__ABGAS000", first_entry[:id]
 
 		assert_equal 48, first_entry[:entries].count
+	end
+end
+
+class Bob
+	def process total_entries:
+		units = []
+		electricity_generated_this_day = []
+		#parallel each here actually increases the time to 50 seconds from 17
+		total_entries.each do |line|
+			electricity_generated = ElectricityGeneration.new(line:line)
+			electricity_generated_this_day.push electricity_generated
+			units.push({id:electricity_generated.unit_id}) unless units.include?({id:electricity_generated.unit_id})
+		end
+
+		units.peach do |unit|
+			unit[:entries] = electricity_generated_this_day.select do |electricity_generated| 
+				electricity_generated.unit_id.eql? unit[:id] 
+			end
+		end
+
+		units
 	end
 end
