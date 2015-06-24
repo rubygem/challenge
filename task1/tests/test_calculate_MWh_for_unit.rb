180252079e3eb4a4b2b0fe7c024e5b8cf33e647f
rubygem <gemma.cameron@gmail.com>
2015-06-24 10:13:25 +0100

    refactoring

--- task1/tests/test_calculate_MWh_for_unit.rb
+++ task1/tests/test_calculate_MWh_for_unit.rb
@@ -7,10 +7,11 @@ class TestCalculateGeneratedElectricity < Test::Unit::TestCase
 	def test_generated_electricity
 		line = CSVFile.new.lines[0]
 		ElectricityGeneration.new(line:line)
-		assert_equal output, "T_RATS-1:     300,000 MWh"
+		output
+		assert_equal @output, "T_RATS-1:     300,000 MWh"
 	end
 
 	def output unit_name:"T_RATS-1", mega_watt_hours:"300,000"
-		"#{unit_name}:     #{mega_watt_hours} MWh"
+		@output = "#{unit_name}:     #{mega_watt_hours} MWh"
 	end
 end
